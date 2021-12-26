ede124b627fb85204e38857536830b11cbe976da
hoshico <melancholinista.yasu@gmail.com>
2021-12-26 14:03:25 +0900

    update Home.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,13 @@
+import { Button, Input } from "@chakra-ui/react"
+import { ChangeEvent, useEffect, useState } from "react"
 import axios from "axios"
-import { useEffect } from "react"
 
 
 export const Home = () => {
-    
+    const [playerName, setPlayerName] = useState("");
+    const onChangePlayerName = (e :ChangeEvent<HTMLInputElement>) => {
+        setPlayerName(e.target.value);
+    }
     useEffect(() => {
         axios.get(`http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='betts%25'`)
              .then((res) => {
@@ -12,7 +16,8 @@ export const Home = () => {
     })
     return (
         <div>
-            
+            <Input placeholder="選手名を入力" value={playerName} onChange={onChangePlayerName}/>
+            <Button >検索</Button>
         </div>
     )
 }
